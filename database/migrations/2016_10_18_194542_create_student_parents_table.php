fa2cc70566528ff6d971470fe694bbc4bc8295dd
Alvaro Guimaraes <alvaroguimaraes@gmail.com>
2016-10-18 22:25:17 +0200

    Laravel 5.3 and basic models.

---
+++ database/migrations/2016_10_18_194542_create_student_parents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentParentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_parents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 150);
+            $table->date('birthdate');
+            $table->string('email', 150)->unique();
+            $table->string('phone', 30);
+            $table->string('address', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_parents');
+    }
+}
